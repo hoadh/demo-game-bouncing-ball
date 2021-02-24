3139d6128f9a234d199ace8b7f24311ed1e508d2
Hoa Dang <danghuyhoa@gmail.com>
2021-02-24 20:35:06 +0700

    Check collision between circle (ball) and rectangle (bar)

--- script.js
+++ script.js
@@ -51,7 +51,7 @@ class Game {
     init() {
         let bar_width = 100;
         let bar_height = 10;
-        this.bar_speed = 10;
+        this.bar_speed = 20;
         let x = this.game_screen.width / 2 - bar_width / 2;
         let y = this.game_screen.height - bar_height;
         this.bar = new Bar(x, y, bar_width, bar_height, "blue");
@@ -61,10 +61,40 @@ class Game {
         let ball_x = this.game_screen.width / 2 - ball_radius / 2;
         let ball_y = this.game_screen.height / 2 - ball_radius / 2;
         this.ball = new Ball(ball_x, ball_y, ball_radius, "red");
-        this.dx = 10;
-        this.dy = 10;
+        this.dx = 2;
+        this.dy = 2;
         
         this.redraw_screen();
+    }
+
+    // Hàm phát hiện va chạm giữa hình tròn và hình chữ nhật
+    isCollision(ball, bar) {
+        let Ax = ball.x;
+        let Ay = ball.y;
+    
+        let rect_left = bar.x;
+        let rect_top = bar.y;
+        let rect_right = bar.x + bar.width;
+        let rect_bottom = bar.x + bar.width;
+    
+        if (ball.x < rect_left)
+            Ax = rect_left;
+        else if (ball.x > rect_right)
+            Ax = rect_right;
+    
+        if (ball.y < rect_top)
+            Ay = rect_top;
+        else if (ball.y > rect_bottom)
+            Ay = rect_bottom;
+    
+        let dx = ball.x - Ax;
+        let dy = ball.y - Ay;
+    
+        return (dx * dx + dy * dy) <= ball.radius * ball.radius;
+    }
+
+    change_ball_direction() {
+        this.dy = -this.dy;
     }
 
     clear_screen() {
@@ -81,10 +111,15 @@ class Game {
         // Lặp lại thao tác vẽ màn hình sau 1 khoảng thời gian
         setInterval(function() {
             self.clear_screen();
+
+            if (self.isCollision(self.ball, self.bar)) {
+                self.change_ball_direction();
+            }
+
             self.move_ball();
             self.bar.draw(self.context);
             self.ball.draw(self.context);
-        }, 100);
+        }, 20);
     }
 
     handle_keyboard(window) {
