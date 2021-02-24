9cca324114322a4ba049ed5f7fc24f837069288b
Hoa Dang <danghuyhoa@gmail.com>
2021-02-24 19:57:08 +0700

    Handle key down for moving bar (move left and move right)

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ class Game {
     init() {
         let bar_width = 100;
         let bar_height = 10;
+        this.bar_speed = 10;
         let x = this.game_screen.width / 2 - bar_width / 2;
         let y = this.game_screen.height - bar_height;
         this.bar = new Bar(x, y, bar_width, bar_height, "blue");
@@ -43,7 +44,29 @@ class Game {
     clear_screen() {
         this.context.clearRect(0, 0, this.game_screen.width, this.game_screen.height);
     }
+
+    handle_keyboard(window) {
+        let self = this;
+        window.addEventListener('keydown', function(event) {
+            let key = event.keyCode;
+
+            self.clear_screen();
+
+            switch(key) {
+                case 37: // left arrow key
+                    self.bar.move(-self.bar_speed);
+                    break;
+                case 39: // right arrow key
+                    self.bar.move(self.bar_speed);
+                    break;
+            }
+
+            self.bar.draw(self.context);
+            
+        });
+    }
 }
 
 let game = new Game();
 game.init();
+game.handle_keyboard(window);
